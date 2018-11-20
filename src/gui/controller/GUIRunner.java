8f7f798c40bfdc035da350546676bdcf77d07fbd
Bowen <tbow1382@740s-pl-120.local>
2018-11-20 08:49:18 -0700

    Created a frame (gui.view.FirstFrame), panel (gui.view.FirstPanel), and controller (gui.controller.GUIController) and a runner (gui.controller.GUIRunner)
    FirstFrame is finished;
    FirstPanel is used to change the window and is not finished;
    GUIRunner is finished;
    GUIController is not finished;

--- src/gui/controller/GUIRunner.java
+++ src/gui/controller/GUIRunner.java
@@ -1,6 +1,13 @@
 package gui.controller;
 
+import gui.model.Duck;
+
 public class GUIRunner
 {
+	public static void main(String [] args)
+	{
+		GUIController app = new GUIController();
+		app.start();
+	}
 
 }
--- src/gui/view/FirstFrame.java
+++ src/gui/view/FirstFrame.java
@@ -1,6 +1,29 @@
 package gui.view;
 
-public class FirstFrame
+import javax.swing.JFrame;
+import gui.controller.GUIController;
+
+public class FirstFrame extends JFrame
 {
 
+	private GUIController appController;
+	private FirstPanel appPanel;
+	
+	public FirstFrame(GUIController appController)
+	{
+		super();
+		this.appController = appController;
+		this.appPanel = new FirstPanel(appController);
+		
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setContentPane(appPanel);;
+		this.setSize(800,800);;
+		this.setTitle("MyGui");
+		this.setResizable(true);;
+		this.setVisible(true);
+	}
 }
